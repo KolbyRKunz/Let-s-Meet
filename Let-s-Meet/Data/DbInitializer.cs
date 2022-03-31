8e56f4ac3bfc4f37bdc3040674f3f9e0c17b4910
Quantumplatr <35316952+Quantumplatr@users.noreply.github.com>
2022-03-30 21:27:54 -0600

    Super Basic Database Seeding Implemented

--- Let-s-Meet/Data/DbInitializer.cs
+++ Let-s-Meet/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Let_s_Meet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +26,123 @@ namespace Let_s_Meet.Data
 
             // and so on
 
+
+            // Create some users
+            List<UserModel> users = CreateUsers(context);
+
+            // Create some groups
+            List<GroupModel> groups = CreateGroups(context, users);
+
+            // Create some events
+            List<EventModel> events = CreateEvents(context, groups);
+
+            context.SaveChanges();
+
+        }
+
+        /// <summary>
+        /// Creates some events and adds them to the database
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="groups"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private static List<EventModel> CreateEvents(MeetContext context, List<GroupModel> groups)
+        {
+            List<EventModel> events = new List<EventModel>();
+
+            // Create an event per group
+            for (int i = 0; i < groups.Count; i++)
+            {
+                // Create event
+                EventModel eventModel = new EventModel
+                {
+                    Group = groups[i],
+                    //Title = "Event " + i,
+                    //Description = "Description " + i,
+                    startTime = DateTime.Now.AddDays(i),
+                    endTime = DateTime.Now.AddDays(i).AddHours(2),
+                    //Location = "Location " + i
+                };
+
+                // Add event to list
+                events.Add(eventModel);
+            }
+
+            // Add events to DB
+            context.Events.AddRange(events);
+
+            return events;
+        }
+
+        /// <summary>
+        /// Creates some groups with users and adds them to the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private static List<GroupModel> CreateGroups(MeetContext context, List<UserModel> users)
+        {
+            List<GroupModel> groupModels = new List<GroupModel>();
+
+            int numGroups = 4;
+
+            // Create some groups
+            for (int i = 0; i < numGroups; i++)
+            {
+                HashSet<UserModel> groupMembers = new HashSet<UserModel>();
+
+                // Add some random users to the group
+                for (int j = 0; j < 3; j++)
+                {
+                    int randomUserIndex = new Random().Next(users.Count);
+                    groupMembers.Add(users[randomUserIndex]);
+                }
+
+                // Create the group
+                GroupModel group = new GroupModel()
+                {
+                    //Name = "Group " + i,
+                    //Description = "This is group " + i,
+                    Users = groupMembers
+                };
+
+                // Add the group to the list of groups
+                groupModels.Add(group);
+            }
+
+            // Add the groups to the DB
+            context.Groups.AddRange(groupModels);
+
+            return groupModels;
+        }
+
+        /// <summary>
+        /// Creates users and adds them to the DB
+        /// </summary>
+        /// <param name="context"></param>
+        public static List<UserModel> CreateUsers(MeetContext context)
+        {
+            List<UserModel> users = new List<UserModel>();
+
+            string[] firstNames = new string[] { "John", "Jane", "Mary", "Mark", "Tom" };
+            string[] lastNames = new string[] { "Smith", "Johnson", "Williams", "Jones", "Brown" };
+
+            // For each name, add a new user
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                users.Add(new UserModel
+                {
+                    FirstName = firstNames[i],
+                    LastName = lastNames[i],
+                    //Email = firstNames[i].ToLower() + lastNames[i].ToLower() + "@gmail.com",
+                    //Password = "password"
+                });
+            }
+
+            // Add the users to the DB
+            context.Users.AddRange(users);
+
+            return users;
         }
     }
 }
--- Let-s-Meet/Models/GroupModel.cs
+++ Let-s-Meet/Models/GroupModel.cs
@@ -11,10 +11,12 @@ namespace Let_s_Meet.Models
         public int ID { get; set; }
 
         [JsonProperty(PropertyName = "GroupSize")]
-        public int GroupSize { get; set; }
+        public int GroupSize { get; set; } // TODO this likely isn't necessary since we can just get the length of the list of users
 
         [JsonProperty(PropertyName = "Users")]
         public ICollection<UserModel> Users { get; set; }
+
+        // TODO have fields like name, description, owner, and members?
 
     }
 }
