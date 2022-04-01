4ec549f9e4ea39e796e085a3c1ad330f2ae787e7
Hope Welch <hopefully.purple@gmail.com>
2022-04-01 15:30:37 -0600

    Buttons grab user data from database
    Successfully connect to the database, request a list of the users, return and parse the result and output data into output box

--- Let-s-Meet/Controllers/APIController.cs
+++ Let-s-Meet/Controllers/APIController.cs
@@ -1,4 +1,5 @@
-﻿using Let_s_Meet.Models;
+﻿using Let_s_Meet.Data;
+using Let_s_Meet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,16 +7,32 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Let_s_Meet.Controllers
 {
     public class APIController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MeetContext _context;
 
-        public APIController(ILogger<HomeController> logger)
+        //public OpportunitiesController(URC_Context context)
+        //{
+        //    _context = context;
+        //}
+
+        // GET: Opportunities
+        //[AllowAnonymous]
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Opportunities.ToListAsync());
+        //}
+
+        public APIController(ILogger<HomeController> logger, MeetContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +49,17 @@ namespace Let_s_Meet.Controllers
         public async Task<IActionResult> Post(string data)
         {
             return Ok(new { message = ("Hello from web server!!" + data) });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> get(string data)
+        {
+            //await _context.Opportunities.ToListAsync()
+            //return Ok(new { message = ("Get???" + data) });
+            var element = await _context.Users.ToListAsync();
+
+            //return Ok(element);
+            return Ok(JsonConvert.SerializeObject(element));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
