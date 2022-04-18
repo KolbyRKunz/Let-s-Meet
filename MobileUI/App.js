317c684c0a6cc886ca0602547fb2fca5cf43b2a0
LindsayWilde <lindsaywilde439@gmail.com>
2022-04-18 15:33:02 -0600

    Calendar API added to mobile end

--- MobileUI/App.js
+++ MobileUI/App.js
@@ -1,5 +1,3 @@
-//@ts-check
-
 import 'react-native-gesture-handler';
 import React from "react";
 import { Text, StatusBar } from 'react-native';
@@ -7,16 +5,92 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import HttpExample from './http_example.js'
 import Demo from './demo.js'
-import Navigator from './routes/drawer';
+//import Navigator from './routes/drawer';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import {LocaleConfig} from 'react-native-calendars';
 
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre'
+  ],
+  monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+  dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+  dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+  today: "Aujourd'hui"
+};
+LocaleConfig.defaultLocale = 'fr';
 
+//https://www.npmjs.com/package/react-native-calendars this is the calendar ap
 export default function App () {
   return(
     <>
-      <Text>
-        Hello World;
-      </Text>
-      <Navigator />
+      <Calendar
+        // Initially visible month. Default = now
+        //current={'2012-03-01'}
+        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        //minDate={'2012-05-10'}
+        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        //maxDate={'2012-05-30'}
+        // Handler which gets executed on day press. Default = undefined
+        onDayPress={day => {
+          console.log('selected day', day);
+        }}
+        // Handler which gets executed on day long press. Default = undefined
+        onDayLongPress={day => {
+          console.log('selected day', day);
+        }}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={'yyyy MM'}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={month => {
+          console.log('month changed', month);
+        }}
+        // Hide month navigation arrows. Default = false
+        hideArrows={true}
+        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+        renderArrow={direction => <Arrow />}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={true}
+        // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+        // day from another month that is visible in calendar page. Default = false
+        disableMonthChange={true}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={true}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={true}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={subtractMonth => subtractMonth()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={addMonth => addMonth()}
+        // Disable left arrow. Default = false
+        disableArrowLeft={true}
+        // Disable right arrow. Default = false
+        disableArrowRight={true}
+        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+        disableAllTouchEventsForDisabledDays={true}
+        // Replace default month and year title with custom one. the function receive a date as parameter
+        renderHeader={date => {
+          /*Return JSX*/
+        }}
+        // Enable the option to swipe between months. Default = false
+        enableSwipeMonths={true}
+      />
+
+      {/* look below to see sources for this
+      <Navigator /> */}
     </>
   );
 };
