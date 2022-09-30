8d1859fc801b4a4bac0017f783b8dea5e894fb21
LindsayWilde <lindsaywilde439@gmail.com>
2022-09-30 10:57:36 -0600

    Bug fix with Auth and Login
    User user = await _um.GetUserAsync(User); would also be null, this should hopefully be a bug fix so it correctly gets the data

--- Let-s-Meet/Controllers/AuthController.cs
+++ Let-s-Meet/Controllers/AuthController.cs
@@ -70,6 +70,7 @@ namespace Let_s_Meet.Controllers
                 {
                     new Claim(ClaimTypes.Name, user.UserName),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
                 };
 
                 foreach (var userRole in userRoles)
